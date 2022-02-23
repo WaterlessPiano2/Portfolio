6cc3655563f2597fa5e306ee588f81d9b7313745
Chad Laptop <scderya@gmail.com>
2022-02-23 23:50:17 +0000

    Personalised

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Chad',
+    site_url: 'https://gigachad.dev',
+    feed_url: 'https://gigachad.dev/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
