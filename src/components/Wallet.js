75079ccfbc3d8376e06d06d86ad0f218eb6a99bb
Chad Laptop <scderya@gmail.com>
2022-03-06 20:01:17 +0100

    small updates

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -25,7 +25,8 @@ require('@solana/wallet-adapter-react-ui/styles.css')
 
 const Wallet = ({ nfts }) => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-  const network = WalletAdapterNetwork.Devnet
+  const network = WalletAdapterNetwork.mainnet-beta
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
 
   // You can also provide a custom RPC endpoint.
   const endpoint = useMemo(() => clusterApiUrl(network), [network])
